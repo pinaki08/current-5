e94deed3b3b8108c52a851db2c46b575b875136c
pinakighosh94 <pinakiranjanghosh2022@gmail.com>
2022-09-05 21:24:23 +0530

    newly study

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import HeaderNew from "./Comp/NewGroup";
 import Group from "./Comp/Headernew";
 import Styleing from "./Comp/Style";
+import { NewExp } from "./Comp/Newexp";
 function Boss() {
   return (
     <div className="study">
       <Group /> <h1> Enjoy your study </h1>
+      <NewExp />
       <Main />
       <HeaderNew />
-      <Styleing/>
+      <Styleing />
     </div>
   );
 }
