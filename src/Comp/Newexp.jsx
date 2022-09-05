c26f1bad61276a76f6887dcc203dae2aad60e8ab
pinakighosh94 <pinakiranjanghosh2022@gmail.com>
2022-09-05 21:56:38 +0530

    exp1

--- src/Comp/Newexp.jsx
+++ src/Comp/Newexp.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export function NewExp() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
-
+  const [tutorial, settutorial] = useState("below");
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1> Click {tutorial} to view new modules </h1>
+      <button onClick={() => settutorial(tutorial + 1)}> clicktab</button>
     </div>
   );
 }
